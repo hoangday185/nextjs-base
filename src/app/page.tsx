a3058fc0fc4a6c0db0cbd76eea3f64725f1fd347
hoangday185 <jarvietnguyen318@gmail.com>
2025-04-15 01:06:36 +0700

    feat: play with image nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
 	return (
-		<main className="font-main">
-			Xin chào các bạn, đây là trang chủ của chúng tôi!
+		<main className="w-[700px] h-[700px] bg-red-300">
+			<Image
+				// src={"/images.png"}
+				src={
+					"https://bs-uploads.toptal.io/blackfish-uploads/components/open_graph_image/8960950/og_image/optimized/1015_Next.js_vs._React-_A_Comparative_Tutorial_Illustration_Brief_Social-d04df761f8138010d9d98703e77ce0e9.png"
+				}
+				width={200}
+				height={200}
+				alt="suffer"
+				quality={100}
+			/>
 		</main>
 	);
 }
